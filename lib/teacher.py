f8247c92e11deea35c5aca0287aa3a3d5fee3d6a
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2023-12-17 07:45:23 -0500

    Completed Assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,22 @@
-#!/usr/bin/env python
-
 from user import User
-
 import random
 
 class Teacher(User):
+    knowledge = [
+        "str is a data type in Python",
+        "programming is hard, but it's worth it",
+        "JavaScript async web request",
+        "Python function call definition",
+        "object-oriented teacher instance",
+        "programming computers hacking learning terminal",
+        "pipenv install pipenv shell",
+        "pytest -x flag to fail fast",
+    ]
+
+    def __init__(self, first_name, last_name):
+        # Call the __init__ method of the parent class (User)
+        super().__init__(first_name, last_name)
 
     def teach(self):
-        pass
+        # Return a random element from the knowledge list
+        return random.choice(self.knowledge)
